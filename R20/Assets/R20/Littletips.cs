91f2806b1f4163277122ab102985e0671594850c
Teddy2024 <teddy31120@gmail.com>
2022-10-21 11:28:35 +0800

    good

---
+++ R20/Assets/R20/Littletips.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace NotToMessUpWithOther
+{
+    ///<summary>
+    ///程式總結
+    ///</summary>
+    public class Littletips : MonoBehaviour
+    {
+      #region 可縮放
+      //123
+      //縮排 Tab
+      /*
+      *1多排註解
+      *2
+      *3
+      *4
+      *5
+      */
+     #endregion
+    }
+}
+
